3d2354e61424970b3a1c6b2f1139cdd6eb846d22
carfox <carlotreball@gmail.com>
2023-08-14 22:24:43 -0500

    Integracion Angular con Flask

---
+++ src/app/vuelos/vuelos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { VuelosService } from '../services/vuelos.service';
+import { Vuelo } from '../shared/models/vuelo.model';
+
+@Component({
+  selector: 'app-vuelos',
+  templateUrl: './vuelos.component.html',
+  styleUrls: ['./vuelos.component.scss']
+})
+export class VuelosComponent implements OnInit, OnDestroy{
+
+  vuelosListaSubs: Subscription;
+  vuelosLista: Vuelo[];
+
+  constructor(private route: ActivatedRoute,
+    private vuelosAPI: VuelosService
+    ) { }
+
+  ngOnInit(): void {
+    this.vuelosListaSubs = this.vuelosAPI
+      .getVuelos()
+      .subscribe( res => {
+        this.vuelosLista = res
+      },
+      console.error
+      );
+    let datos = this.route.snapshot.params;
+    console.log(datos);
+  }
+
+  ngOnDestroy(): void {
+    this.vuelosListaSubs.unsubscribe()
+  }
+}
