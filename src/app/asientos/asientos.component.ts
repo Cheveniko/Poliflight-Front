91d6f5a6c65980a434b1369f6220a215e939c214
cheveniko <nicolas.baquero@icloud.com>
2023-08-22 01:22:51 -0500

    Componente: añade componente asientos

--- src/app/asientos/asientos.component.ts
+++ src/app/asientos/asientos.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-asientos',
   templateUrl: './asientos.component.html',
-  styleUrls: ['./asientos.component.scss']
+  styleUrls: ['./asientos.component.scss'],
 })
 export class AsientosComponent {
-
+  config = {
+    region: 'sa', // e.g. "eu"
+    workspaceKey: '26b63090-3ae6-4517-ac96-f899acb49252',
+    event: 'Poliflight',
+    session: 'continue',
+    language: 'es',
+    categoryFilter: { enabled: true, zoomOnSelect: true },
+    pricing: [
+      {
+        category: 1,
+        ticketTypes: [
+          { ticketType: 'adult', price: 30 },
+          { ticketType: 'child', price: 20 },
+        ],
+      },
+      {
+        category: 2,
+        price: 50,
+      },
+    ],
+    priceFormatter: (price: number) => '$' + price,
+    selectedObjectsInputName: 'selectedSeatsField',
+    holdTokenInputName: '',
+  };
 }
