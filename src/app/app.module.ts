b2e7421e14f2d44f6e6671952830e0d7836f6025
cheveniko <nicolas.baquero@icloud.com>
2023-08-22 01:22:31 -0500

    Dependencias: añade modulo seats io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { FormulariosModule } from './formularios/formularios.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+//Librerias
+import { SeatsioAngularModule } from '@seatsio/seatsio-angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 //Componentes
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { VuelosComponent } from './vuelos/vuelos.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { VuelosService } from './services/vuelos.service';
 import { RegistroComponent } from './registro/registro.component';
 import { AsientosComponent } from './asientos/asientos.component';
@@ -33,6 +37,7 @@ import { AsientosComponent } from './asientos/asientos.component';
     FormulariosModule,
     FontAwesomeModule,
     HttpClientModule,
+    SeatsioAngularModule,
   ],
   providers: [VuelosService],
   bootstrap: [AppComponent],
